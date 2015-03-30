5cc049bb43663ebab8b8760659bbcfa4a5731e90
Silver Zachara <silver.zachara@gmail.com>
2015-03-30 22:46:51 +0200

    aktualizovaný host a namespace pre RESTAdapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'http://symfony-rest-edition.dev',
-  namespace: 'app_dev.php/api'
+  host: 'http://symfony-rest-edition.dev/app_dev.php',
+  namespace: 'api'
 });
