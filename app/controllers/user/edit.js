aae7d8650ab8494b2850b5a16c823197cba09945
Silver Zachara <silver.zachara@gmail.com>
2015-04-06 21:47:25 +0200

    odstránení nepotrebný parameter

--- app/controllers/user/edit.js
+++ app/controllers/user/edit.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   actions: {
-    submit: function(test) {
+    submit: function() {
       var model = this.get('model');
 
       if (model.get('isDirty')) {
